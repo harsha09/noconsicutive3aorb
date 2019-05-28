0622f977b1d25f73907b3b96715565c7efee22c0
harsha09 <harsha.bajju@gmail.com>
2019-05-28 23:49:35 +0530

    Add missing validation.

--- src/unbabel_cli.py
+++ src/unbabel_cli.py
@@ -2,14 +2,29 @@ from fire import Fire
 from aggregate import run
 from os.path import split, exists
 
+
 def read_input(input_file, out_file, window_size, breakdown=1):
     """
     Args:
-        input_file (string): Provide the full path of events file.
-        out_file (string): Provide the full path of output file.
-        window_size (number): Provide the window size in minutes.
-        breakdown (number): Provide breakdown in minutes should be less than window_size.
+        input_file (string): provide the full path of events file
+        out_file (string): provide the full path of output file
+        window_size (number): provide the window size in minutes
+        breakdown (number): provide breakdown in minutes should be less than window_size
+    Raises:
+        FileNotFoundError: if input file or output folder doesn't exist
+        ValueError: if breakdown or window_size are not integers
+                    if window_size < breakdown
+                    if breakdown or window_size are < 1
+    Returns:
+        string: always Done...
     """
+
+    if not exists(input_file):
+        raise FileNotFoundError('Please provide valid input file path.')
+
+    if not exists(split(out_file)[0]):
+        raise FileNotFoundError('Please provide path to write output file.')
+
     if breakdown < 1 or not isinstance(breakdown, int):
         raise ValueError('Breakdown must be an integer and greater than 0.')
 
